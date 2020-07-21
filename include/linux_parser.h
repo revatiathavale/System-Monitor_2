fc8ccfef218a139c322b38551dd1d584d78b69dd
revatiathavale <57042399+revatiathavale@users.noreply.github.com>
2020-07-21 12:01:56 -0400

    Update linux_parser.h

--- include/linux_parser.h
+++ include/linux_parser.h
@@ -40,7 +40,7 @@ enum CPUStates {
   kGuest_,
   kGuestNice_
 };
-std::vector<std::string> CpuUtilization();
+float CpuUtilization();
 long Jiffies();
 long ActiveJiffies();
 long ActiveJiffies(int pid);
@@ -51,7 +51,7 @@ std::string Command(int pid);
 std::string Ram(int pid);
 std::string Uid(int pid);
 std::string User(int pid);
-long int UpTime(int pid);
+long UpTime(int pid);
 };  // namespace LinuxParser
 
 #endif
